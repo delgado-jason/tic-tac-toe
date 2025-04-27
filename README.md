472ba0e29ce75367c05c8006c56d3559972fccd4
delgado-jason <delgadorjason7@gmail.com>
2025-04-26 21:42:34 -0500

    Start building the modules and functions for the game and update the README.md file.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# tic-tac-toe
+# Tic-Tac-Toe
+
+## How The Game is Played
+The game is played by 2 players. They represent either 'X' or 'O'. Whether a player is 'X' or 'O' is completely random and player 'X' is the first to go.
+
+The first player to get three of their tokens in a row, column, or diagnoly wins. This game is considered futile, because two experienced players will almost always result in a draw.
+
+## The Goal
+The goal of this project is to use vanilla Javascript to build the game. More specifically, use **factory functiions** and **IIFE's (Immediately-invoked Function Expressions)**.
--- main.js
+++ main.js
@@ -1,1 +1,37 @@
-console.log('I am working from main.js')
+// The Gameboard
+const gameboard = (function () {
+    let gameboard = [
+    ['[]', '[]', '[]'],
+    ['[]', '[]', '[]'],
+    ['[]', '[]', '[]']
+];
+
+    const getGameboard = () => gameboard;
+
+    return {getGameboard}
+})()
+
+// Player
+function player(name) {
+    name;
+    playerToken = token();
+
+    const getPlayerToken = () => playerToken;
+
+    return {name, getPlayerToken};
+}
+
+// Player tokens
+function token() {
+    // Generate token
+    if (player.playerToken === 'X') {
+            token = 'O';
+        } else {
+            token = 'X'
+        }
+
+    
+    
+    return {token}
+} 
+
