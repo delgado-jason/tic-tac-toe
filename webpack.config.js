9d2beec548928e0bb884d20b4712d3d9f014a892
delgado-jason <delgadorjason7@gmail.com>
2025-05-06 11:08:06 -0400

    Get Webpack Setup

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    plugins: [new HtmlWebpackPlugin({
+            title: 'Tic-Tac-Toe',
+            template: './src/assets/template.html',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+        ],
+
+    },
+};
